24e8fefc7f4b2891c0537c98816367f47764c257
Luis <lfr4704@gmail.com>
2018-04-25 10:28:47 -0700

    updated

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,14 +1,11 @@
 
 
 
-from django.contrib import admin
+from . import views
 from django.urls import include, path
 #solution from https://spapas.github.io/2015/04/29/django-show-404-page/
 import django.views.defaults
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    #
-    url(r'^404/$', django.views.defaults.page_not_found, ),
+    path('test/', views.index)
 ]
