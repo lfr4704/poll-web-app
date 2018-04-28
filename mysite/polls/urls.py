77aafb82d9acb389b9eec6f3363ca0b2a8a8d8b3
Luis <lfr4704@gmail.com>
2018-04-28 09:09:28 -0700

    updated files, completed part 2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,11 +1,7 @@
 
-
-
+from django.urls import path
 from . import views
-from django.urls import include, path
-#solution from https://spapas.github.io/2015/04/29/django-show-404-page/
-import django.views.defaults
 
 urlpatterns = [
-    path('test/', views.index)
+    path('', views.index, name='index') #function with four arguments
 ]
